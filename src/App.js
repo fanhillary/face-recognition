4a73bdd4c8243d66a26852248c00dff3b46683a5
Hillary Fan <hillary.fan123@gmail.com>
2019-12-03 12:15:18 -0800

    added particles js and changed font family

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import './App.css';
 import Navigation from './Navigation/Navigation';
 import Logo from './Logo/Logo';
 import ImageLinkForm from './ImageLinkForm/ImageLinkForm';
+import Rank from './Rank/Rank';
+import Particles from 'react-particles-js';
 
+const particlesOptions = {
+  particles: {
+    number: { 
+      value: 80,
+      density: {
+        enable: true, 
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+       <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm/>
       {/* <FaceRecognition/> */}
     </div>
